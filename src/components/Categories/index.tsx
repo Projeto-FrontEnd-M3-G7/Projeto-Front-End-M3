a781977dced136069e768352f26c4263399a17de
Fernandomtos <rmavali@bol.com.br>
2023-03-08 18:27:12 -0300

    feat: css menu categorias mobile

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,25 +1,21 @@
 import { StyledAside } from "./styles";
 
-export const Categories = () => {
-  console.log("TESTE");
-
-  return (
-    <StyledAside>
-      <h2>Categorias</h2>
-      <input type="text" placeholder="buscar categoria" />
-      <img src="./src/assets/img/icons8-pesquisar.png" alt="botão de busca" />
-      <ul>
-        <li>Eletrônicos</li>
-        <li>Calçados</li>
-        <li>Roupas</li>
-        <li>Eletrodomésticos</li>
-        <li>Informática</li>
-        <li>Eletrônicos</li>
-        <li>Calçados</li>
-        <li>Roupas</li>
-        <li>Eletrodomésticos</li>
-        <li>Informática</li>
-      </ul>
-    </StyledAside>
-  );
-};
+export const Categories = () => (
+  <StyledAside>
+    <h2>Categorias</h2>
+    <input type="text" placeholder="buscar categoria" />
+    <img src="./src/assets/img/icons8-pesquisar.png" alt="botão de busca" />
+    <ul>
+      <li>Eletrônicos</li>
+      <li>Calçados</li>
+      <li>Roupas</li>
+      <li>Eletrodomésticos</li>
+      <li>Informática</li>
+      <li>Eletrônicos</li>
+      <li>Calçados</li>
+      <li>Roupas</li>
+      <li>Eletrodomésticos</li>
+      <li>Informática</li>
+    </ul>
+  </StyledAside>
+);
--- src/components/Categories/styles.ts
+++ src/components/Categories/styles.ts
@@ -1,62 +1,79 @@
 import styled from "styled-components";
 
 export const StyledAside = styled.aside`
-  display: none;
+  height: 318px;
+  margin: 10px;
+  background-color: var(--color-primary);
+  padding: 15px;
+  opacity: 80%;
+  font-size: var(--title4);
+  color: var(--grey-0);
+  border-radius: var(--radius-input);
+  position: relative;
+  display: flex;
+  flex-direction: column;
 
-  @media (min-width: 769px) {
-    background-color: var(--color-primary);
-    opacity: 80%;
-    width: 259px;
-    height: 618px;
-    padding: 20px;
+  h2 {
     font-size: var(--title4);
-    color: var(--grey-0);
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  input {
+    width: 100%;
+    min-height: 42px;
+    border-radius: var(--radius-input);
+    border: none;
+    padding-left: 8.8px;
+    margin-bottom: 16px;
+  }
+
+  img {
+    width: 30px;
+    height: 22px;
+    object-fit: contain;
+    position: absolute;
+    top: 65px;
+    right: 25px;
+  }
+
+  ul {
     display: flex;
     flex-direction: column;
-    border-radius: var(--radius-input);
-    margin-right: 28px;
+    gap: 11.64px;
+    flex-wrap: wrap;
+    overflow-y: auto;
 
-    ul {
-      display: flex;
-      flex-direction: column;
-      gap: 11.64px;
+    li {
+      transition: transform 1s;
+      transform: translateX(0) scale(1);
+      cursor: pointer;
 
-      li {
-        transition: transform 1s;
-        transform: translateX(0) scale(1);
-        cursor: pointer;
-
-        :hover,
-        :focus {
-          transform: translateX(20px) scale(1.2);
-          filter: brightness(80%);
-        }
+      :hover,
+      :focus {
+        transform: translateX(20px) scale(1.2);
+        filter: brightness(80%);
       }
     }
+  }
+
+  @media (min-width: 769px) {
+    width: 259px;
+    height: 618px;
+    margin-right: 28px;
+    margin-top: 0;
 
     h2 {
-      font-size: var(--title4);
-      text-align: center;
       margin-bottom: 36.06px;
     }
 
     input {
-      width: 219px;
-      height: 42px;
-      border-radius: var(--radius-input);
-      border: none;
-      padding-left: 8.8px;
       margin-bottom: 26px;
-      position: relative;
     }
 
     img {
-      width: 30px;
-      height: 22px;
-      object-fit: contain;
-      position: absolute;
-      top: 240px;
-      left: 219px;
+      top: 85px;
+      left: 205px;
     }
   }
 `;
