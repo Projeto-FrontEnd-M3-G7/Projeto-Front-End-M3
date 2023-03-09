db7e8058c8c42bcdf28a918249be6d435cdfb3b7
Cleyviton <cleyvitonkeke@gmail.com>
2023-03-09 16:15:55 -0300

    feat: form in progess

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,34 +1,30 @@
-import { CardMyProducts } from '../../components/MyProducts';
-import { Header } from '../../components/Header';
-import { DivContainer } from './style';
+import { CardMyProducts } from "../../components/MyProducts";
+import { Header } from "../../components/Header";
+import { DivContainer } from "./style";
 
-export const Dashboard = () => {
-    return (
-        <DivContainer>
-            <Header />
-            <main>
-                <section className="user">
-                    <div className="userInfos">
-                        <h2>NOME DO USUARIO</h2>
-                        <div className="userDados">
-                            <p>E-mail:</p>
-                            <p>Plano:</p>
-                            <p>DATA:</p>
-                        </div>
-                    </div>
+export const Dashboard = () => (
+  <DivContainer>
+    <Header />
+    <main>
+      <section className="user">
+        <div className="userInfos">
+          <h2>NOME DO USUARIO</h2>
+          <div className="userDados">
+            <p>E-mail:</p>
+            <p>Plano:</p>
+            <p>DATA:</p>
+          </div>
+        </div>
 
-                    <div className="editUser">
-                        <button>Editar</button>
-                    </div>
-                </section>
-                <aside className="myProducts">
-                    <h2 className="myProducts__title">
-                        Controle de produtos Ofertados
-                    </h2>
+        <div className="editUser">
+          <button>Editar</button>
+        </div>
+      </section>
+      <aside className="myProducts">
+        <h2 className="myProducts__title">Controle de produtos Ofertados</h2>
 
-                    <CardMyProducts />
-                </aside>
-            </main>
-        </DivContainer>
-    );
-};
+        <CardMyProducts />
+      </aside>
+    </main>
+  </DivContainer>
+);
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import { StyledMain } from "./styles";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { Categories } from "../../components/Categories";
 import { ProductList } from "../../components/ProductList";
+import { ModalLogin } from "../../components/Modais/ModalLogin";
+import { ModalRegister } from "../../components/Modais/ModalRegister";
 
 export const HomePage = () => {
-  console.log("deletar depois");
+  const [isOpenModalLogin, setIsOpenModalLogin] = useState(false);
+  const [isOpenModalRegister, setIsOpenModalRegister] = useState(false);
 
   return (
     <>
-      <Header labelBtn1="Login" labelBtn2="Cadastrar" />
+      {isOpenModalLogin && (
+        <ModalLogin
+          setIsOpenModalLogin={setIsOpenModalLogin}
+          setIsOpenModalRegister={setIsOpenModalRegister}
+        />
+      )}
+      {isOpenModalRegister && (
+        <ModalRegister
+          setIsOpenModalLogin={setIsOpenModalLogin}
+          setIsOpenModalRegister={setIsOpenModalRegister}
+        />
+      )}
+      <Header
+        labelBtn1="Login"
+        labelBtn2="Cadastrar"
+        setIsOpenModalLogin={setIsOpenModalLogin}
+        setIsOpenModalRegister={setIsOpenModalRegister}
+      />
       <StyledMain>
         <div className="boxMain">
           <div className="containerCategories">
